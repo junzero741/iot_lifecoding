702cb5ac30940e994ed7180d48d809299c3487ad
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-09-21 13:07:33 +0900

    생활코딩에서 iot를 연관지어 코딩을 배움

---
+++ ClassApp.java
@@ -0,0 +1,14 @@
+
+public class ClassApp {
+
+	public static void main(String[] args) {
+		
+		// Class example 
+		System.out.println(Math.PI);			// 3.14
+		System.out.println(Math.floor(1.6)); 	// 1.0
+		System.out.println(Math.ceil(1.6));		// 2.0
+		
+
+	}
+
+}
